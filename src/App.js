6c8da41f9154c5a2142ac333fc4e87c9df67e033
Philip Hinchsliff <philip.hinchsliff@yahoo.com>
2022-03-17 16:15:27 +0200

    added countdown functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Timer from './components/Timer';
 function App() {
   return (
     <div className="App">
+      <h1>New Years Countdown</h1>
       <Timer />
     </div>
   );
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,21 +5,72 @@ import Box from "./Box";
 
 const Timer = () => {
 
-    const [months, setMonths] = useState(5)
-    const [days, setDays] = useState(261)
-    const [hours, setHours] = useState(15)
-    const [minutes, setMinutes] = useState(47)
-    const [seconds, setSeconds] = useState(21)
+    const [months, setMonths] = useState(0)
+    const [days, setDays] = useState(0)
+    const [hours, setHours] = useState(0)
+    const [minutes, setMinutes] = useState(0)
+    const [seconds, setSeconds] = useState(0)
+
+    const [isCancelled, setIsCancelled] = useState(false)
+
+    const now = new Date()
+    const currentYear = new Date().getFullYear()
+    const newYears = new Date(`01 Jan ${ currentYear + 1 } 00:00:00 GMT`);
+
 
     //MAIN LOGIC
 
     useEffect(() => {
-        setInterval(function () {
-            getTimeAndDateNow();
-        }, 1000);
+
+        const myInterval = setInterval(() => {
+            calculateTimeDifference()
+        }, 1000)
+
+        return () => clearInterval(myInterval)
+
+    }, [seconds])
 
 
 
+    const calculateTimeDifference = () => {
+        //time difference in milliseconds
+        const diff = Date.parse(newYears) - Date.parse(now);
+        //set days left
+        setDays(Math.floor(diff / 1000 / 60 / 60 / 24));
+        //set hours left
+        setHours(Math.floor(diff / 1000 / 60 / 60) % 24);
+        //set minutes left
+        setMinutes(Math.floor(diff / 1000 / 60) % 60);
+        //set seconds left
+        setSeconds(Math.floor(diff / 1000) % 60);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /////////////////////////////////////////////////////
+    useEffect(() => {
+        // setInterval(function () {
+        //     getTimeAndDateNow();
+        // }, 1000);
     }, [])
 
     const getTimeAndDateNow = () => {
@@ -30,19 +81,12 @@ const Timer = () => {
         let min = new Date().getMinutes();
         let sec = new Date().getSeconds();
 
-        let now = new Date()
-
         // day = day < 10 ? '0' + day : day
         // month = month < 10 ? '0' + month : month
         // hour = hour < 10 ? '0' + hour : hour
         // min = min < 10 ? '0' + min : min
         // sec = sec < 10 ? '0' + sec : sec
-
     };
-
-
-
-
 
     return (
         <div className="timerContainer">
